65bdc1012eb479ef54a7ada28cc065e054e72eb8
CybeastID <spypacman@gmail.com>
2024-01-16 18:24:02 -0500

    Changed XP charts for levels beyond 100. Disabled XP candies beyond 100.

--- src/data/pokemon/experience_tables.h
+++ src/data/pokemon/experience_tables.h
@@ -120,6 +120,12 @@ const u32 gExperienceTables[][MAX_LEVEL + 1] =
         EXP_MEDIUM_FAST(98),
         EXP_MEDIUM_FAST(99),
         EXP_MEDIUM_FAST(100),
+        UNFAIR_AI_CONSTANT + EXP_MEDIUM_FAST(101),
+        UNFAIR_AI_CONSTANT + EXP_MEDIUM_FAST(102),
+        UNFAIR_AI_CONSTANT + EXP_MEDIUM_FAST(103),
+        UNFAIR_AI_CONSTANT + EXP_MEDIUM_FAST(104),
+        UNFAIR_AI_CONSTANT + EXP_MEDIUM_FAST(105),
+
     },
     { // Erratic
         0, // 0
@@ -223,6 +229,11 @@ const u32 gExperienceTables[][MAX_LEVEL + 1] =
         EXP_ERRATIC(98),
         EXP_ERRATIC(99),
         EXP_ERRATIC(100),
+        UNFAIR_AI_CONSTANT + EXP_ERRATIC(101),
+        UNFAIR_AI_CONSTANT + EXP_ERRATIC(102),
+        UNFAIR_AI_CONSTANT + EXP_ERRATIC(103),
+        UNFAIR_AI_CONSTANT + EXP_ERRATIC(104),
+        UNFAIR_AI_CONSTANT + EXP_ERRATIC(105),
     },
     { // Fluctuating
         0, // 0
@@ -326,6 +337,11 @@ const u32 gExperienceTables[][MAX_LEVEL + 1] =
         EXP_FLUCTUATING(98),
         EXP_FLUCTUATING(99),
         EXP_FLUCTUATING(100),
+        UNFAIR_AI_CONSTANT,
+        UNFAIR_AI_CONSTANT + EXP_FLUCTUATING(102),
+        UNFAIR_AI_CONSTANT + EXP_FLUCTUATING(103),
+        UNFAIR_AI_CONSTANT + EXP_FLUCTUATING(104),
+        UNFAIR_AI_CONSTANT + EXP_FLUCTUATING(105),
     },
     { // Medium Slow
         0, // 0
@@ -429,6 +445,11 @@ const u32 gExperienceTables[][MAX_LEVEL + 1] =
         EXP_MEDIUM_SLOW(98),
         EXP_MEDIUM_SLOW(99),
         EXP_MEDIUM_SLOW(100),
+        UNFAIR_AI_CONSTANT,
+        UNFAIR_AI_CONSTANT + EXP_MEDIUM_SLOW(102),
+        UNFAIR_AI_CONSTANT + EXP_MEDIUM_SLOW(103),
+        UNFAIR_AI_CONSTANT + EXP_MEDIUM_SLOW(104),
+        UNFAIR_AI_CONSTANT + EXP_MEDIUM_SLOW(105),
     },
     { // Fast
         0, // 0
@@ -532,6 +553,11 @@ const u32 gExperienceTables[][MAX_LEVEL + 1] =
         EXP_FAST(98),
         EXP_FAST(99),
         EXP_FAST(100),
+        UNFAIR_AI_CONSTANT + EXP_FAST(101),
+        UNFAIR_AI_CONSTANT + EXP_FAST(102),
+        UNFAIR_AI_CONSTANT + EXP_FAST(103),
+        UNFAIR_AI_CONSTANT + EXP_FAST(104),
+        UNFAIR_AI_CONSTANT + EXP_FAST(105),
     },
     { // Slow
         0, // 0
@@ -635,6 +661,12 @@ const u32 gExperienceTables[][MAX_LEVEL + 1] =
         EXP_SLOW(98),
         EXP_SLOW(99),
         EXP_SLOW(100),
+        UNFAIR_AI_CONSTANT + EXP_SLOW(101),
+        UNFAIR_AI_CONSTANT + EXP_SLOW(102),
+        UNFAIR_AI_CONSTANT + EXP_SLOW(103),
+        UNFAIR_AI_CONSTANT + EXP_SLOW(104),
+        UNFAIR_AI_CONSTANT + EXP_SLOW(105),
+        
     },
     { // Medium Fast copy 2 (unused? to-do: investigate)
         0, // 0
@@ -738,6 +770,8 @@ const u32 gExperienceTables[][MAX_LEVEL + 1] =
         EXP_MEDIUM_FAST(98),
         EXP_MEDIUM_FAST(99),
         EXP_MEDIUM_FAST(100),
+        1000000,
+        1000000,
     },
     { // Medium Fast copy 3 (unused? to-do: investigate)
         0, // 0
@@ -841,5 +875,7 @@ const u32 gExperienceTables[][MAX_LEVEL + 1] =
         EXP_MEDIUM_FAST(98),
         EXP_MEDIUM_FAST(99),
         EXP_MEDIUM_FAST(100),
+        1000000,
+        1000000,
     }
 };
